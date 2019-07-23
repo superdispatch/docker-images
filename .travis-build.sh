dd5b48bbcce92911acdf9576cb6bbabb60d4f503
Umidbek Karimov <uma.karimov@gmail.com>
2019-07-24 00:31:24 +0500

    chore(.travis.yml): Setup docker login. (#2)

---
+++ .travis-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Node Pipeline
+docker pull superdispatch/node-pipeline || true
+docker build --pull --cache-from superdispatch/node-pipeline --tag superdispatch/node-pipeline node-pipeline
+docker run --rm -v $(pwd)/node-pipeline/test.sh:/test.sh superdispatch/node-pipeline bash test.sh
