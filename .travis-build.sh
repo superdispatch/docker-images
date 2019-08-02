3910dbb70752384807f19d5f8d506e5f8bb87fc7
Umidbek Karimov <uma.karimov@gmail.com>
2019-08-02 17:44:52 +0500

    chore(.travis-build.sh): Add latest tags.

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Node Pipeline
-docker pull superdispatch/node-pipeline || true
-docker build --pull --cache-from superdispatch/node-pipeline --tag superdispatch/node-pipeline node-pipeline
+docker pull superdispatch/node-pipeline:latest || true
+docker build --pull --cache-from superdispatch/node-pipeline:latest --tag superdispatch/node-pipeline node-pipeline
 docker run --rm -v $(pwd)/node-pipeline/test.sh:/test.sh superdispatch/node-pipeline bash test.sh
