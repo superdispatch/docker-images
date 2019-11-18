25d284d20b4ff48f393f2d0b94f39ac4693819cc
Umidbek Karimov <uma.karimov@gmail.com>
2019-11-18 15:34:50 +0500

    chore: Migrate to Circle CI.

--- .travis-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Node Pipeline
-docker pull superdispatch/node-pipeline:latest || true
-docker build --pull --cache-from superdispatch/node-pipeline:latest --tag superdispatch/node-pipeline node-pipeline
-docker run --rm -v $(pwd)/node-pipeline/test.sh:/test.sh superdispatch/node-pipeline bash test.sh
